5b81c578721f6ddbf280cda647d54cc60a2b56e2
NguyenVietHung <nguyenviethung19981312@gmail.com>
2020-06-03 13:08:50 +0700

    edit categories, edit products pending

--- src/components/page/DashBoard/ManageCategories/Categories.js
+++ src/components/page/DashBoard/ManageCategories/Categories.js
@@ -1,14 +1,34 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import { EditOutlined ,DeleteOutlined } from '@ant-design/icons';
-import { Table, Space , Popconfirm, message} from 'antd';
-import Update from './Update';
+import { Table, Space , Popconfirm, message, Modal, Form, Input, Button} from 'antd';
+// import Update from './Update';
 import DashBoard from '../Dashboard';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
 import { useStores } from '../../../../hooks/useStores';
 
+const layout = {
+  labelCol: {
+      span: 4,
+  },
+  wrapperCol: {
+      span: 16,
+  },
+};
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+      email: '${label} is not validate email!',
+      number: '${label} is not a validate number!',
+  },
+  number: {
+      range: '${label} must be between ${min} and ${max}',
+  },
+};
 const Categories = observer(() => {
     const { categoriesStore } = useStores();
+    const [visible,setVisible] = useState(false);
+    const [form] = Form.useForm();
 
     useEffect(() => {
       categoriesStore.getCategories();
@@ -18,6 +38,18 @@ const Categories = observer(() => {
       console.log(e);
       message.error('Click on No');
     }
+
+    const showModal = () => {
+      setVisible(true);
+    };
+    const handleCancel = e => {
+      console.log(e);
+      setVisible(false);
+    };
+    const onFinish = values => {
+      console.log(values);
+    };
+
 
     // console.log(toJS(categoriesStore.categories));
     const categories = categoriesStore.categories;
@@ -34,7 +66,46 @@ const Categories = observer(() => {
         key: 'action',
         render: id => (
           <Space size="middle">
-            <EditOutlined onClick={() => alert(1)} />
+            <EditOutlined  onClick={showModal}/>
+            <Modal
+              title="Edit Categorie name"
+              visible={visible}
+              onCancel={handleCancel}
+            >
+            <Form
+            {...layout}
+            form={form}
+            name="categorie-form"
+            initialValues={{
+              name : '',
+              price : 0,
+              quantity : 0,
+              status : '',
+              description : ''
+            }}
+            onFinish={onFinish}
+            validateMessages={validateMessages}>
+            <Form.Item
+                name="name"
+                label="Name"
+                rules={[
+                  {
+                      minx: 3,
+                      max: 500,
+                      required: true,
+                  },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item style={{ width : '50%', margin : '0 auto' }}>
+              <Button type="primary" htmlType="submit">
+                  Submit
+              </Button>
+            </Form.Item>
+            </Form>
+            </Modal>
+
             <Popconfirm
               title="Are you sure delete this task?"
               onConfirm={()=> {
